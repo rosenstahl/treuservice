82012b6ea238c6f287a183d8a0ae287112bdabd8
rosenstahl <hasanyilmaz1811@gmail.com>
2025-04-03 00:19:43 +0200

    Update StepIndicator.tsx

--- src/components/contact/entkernung/StepIndicator.tsx
+++ src/components/contact/entkernung/StepIndicator.tsx
@@ -44,14 +44,14 @@ export const StepIndicator: React.FC<StepIndicatorProps> = ({ currentStep, total
             >
               {currentStep > step ? (
                 <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4" viewBox="0 0 20 20" fill="currentColor">
-                  <path fillRule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clipRule="evenodd" />
+                    <path fillRule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clipRule="evenodd" />
                 </svg>
               ) : (
                 <span className="text-xs font-medium">{step}</span>
               )}
             </motion.div>
             <div 
-              className={`text-xs mt-2 font-medium text-center px-1
+              className={`hidden md:block text-xs mt-2 font-medium text-center px-1
               ${currentStep === step 
                 ? 'text-[#009FD8]' 
                 : currentStep > step 

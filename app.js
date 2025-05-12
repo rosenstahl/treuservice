ba56e58a65813d439cc3c69215320f09100f92fe
rosenstahl <hasanyilmaz1811@gmail.com>
2025-05-13 00:20:48 +0200

    Füge app.js als expliziten Passenger-Starter hinzu

---
+++ app.js
@@ -0,0 +1,12 @@
+#!/usr/bin/env node
+
+/**
+ * Passenger App Starter
+ * 
+ * Diese Datei dient als expliziter Einstiegspunkt für Phusion Passenger
+ */
+
+console.log('Starting application with Passenger...');
+
+// Importiere den Server
+require('./server.js');
